3833bf5bf5bc3db67626b46d24f77708840232b4
Caian Egidio <caianpedroso@gmail.com>
2024-01-05 23:48:26 -0300

    Crudcomqualidade: aula 2.2 modulo 2

--- crudcomqualidade/Modulo-2/core/crud.ts
+++ crudcomqualidade/Modulo-2/core/crud.ts
@@ -4,17 +4,47 @@ const DB_FILE_PATH = "./db";
 
 console.log('[Crud]');
 
+interface Todo {
+    date: string;
+    content: string;
+    done: boolean;
+}
+
 function create(content: string) {
+    const todo: Todo = {
+        date: new Date().toISOString(),
+        content : content,
+        done: false,
+    }
+
+    const todos: Array<Todo> = [
+        ...read(),
+        todo,
+    ]
+
+    // console.log(todo);
     // salvar o content no sistema
-    fs.writeFileSync(DB_FILE_PATH, content);
+    fs.writeFileSync(DB_FILE_PATH, JSON.stringify({
+        todos
+    }, null, 2));
     return content
 }
 
-function read() {
-    const db = fs.readFileSync(DB_FILE_PATH, "utf-8")
-    return db;
+function read(): Array<Todo> {
+    const dbString = fs.readFileSync(DB_FILE_PATH, "utf-8");
+    const db = JSON.parse(dbString || "{}")
+    if(!db.todos) { // FAIL FAST VALIDATION
+        return []
+    }
+    return db.todos;
+}
+
+function CLEAR_DB() {
+    fs.writeFileSync(DB_FILE_PATH, "")
 }
 
 // SIMULATION
+CLEAR_DB()
+create("Primeira TODO")
 create("Segunda TODO")
 console.log(read())
