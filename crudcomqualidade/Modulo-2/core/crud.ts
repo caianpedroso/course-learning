0faba5f94b7c1135e103d0734c1d17ee1214ef4b
Caian Egidio <caianpedroso@gmail.com>
2024-01-06 01:31:24 -0300

    Crudcomqualidade: aula 2.3 modulo 2 UPDATE

--- crudcomqualidade/Modulo-2/core/crud.ts
+++ crudcomqualidade/Modulo-2/core/crud.ts
@@ -1,17 +1,20 @@
 import fs from "fs"; // ES6
+import { v4 as uuid } from "uuid";
 // const fs = require("fs");
 const DB_FILE_PATH = "./db";
 
 console.log('[Crud]');
 
 interface Todo {
+    id: string;
     date: string;
     content: string;
     done: boolean;
 }
 
-function create(content: string) {
+function create(content: string): Todo {
     const todo: Todo = {
+        id: uuid(),
         date: new Date().toISOString(),
         content : content,
         done: false,
@@ -27,7 +30,7 @@ function create(content: string) {
     fs.writeFileSync(DB_FILE_PATH, JSON.stringify({
         todos
     }, null, 2));
-    return content
+    return todo
 }
 
 function read(): Array<Todo> {
@@ -39,6 +42,33 @@ function read(): Array<Todo> {
     return db.todos;
 }
 
+function update(id: string, partialTodo: Partial<Todo>): Todo {
+    let updateTodo;
+    const todos = read();
+    todos.forEach((currentTodo) => {
+        const isToUpdate = currentTodo.id === id;
+        if(isToUpdate) {
+            updateTodo = Object.assign(currentTodo, partialTodo)
+        }
+    });
+
+    fs.writeFileSync(DB_FILE_PATH, JSON.stringify({
+        todos,
+    }, null, 2))
+
+    if(!updateTodo) {
+        throw new Error("Please, provide another ID !")
+    }
+
+    return updateTodo;
+}
+
+function updateContentById(id: string, content: string): Todo {
+    return update(id, {
+        content,
+    })
+}
+
 function CLEAR_DB() {
     fs.writeFileSync(DB_FILE_PATH, "")
 }
@@ -47,4 +77,10 @@ function CLEAR_DB() {
 CLEAR_DB()
 create("Primeira TODO")
 create("Segunda TODO")
+const terceiraTodo = create("Segunda TODO");
+// update(terceiraTodo.id, {
+//     content: "Segunda TODO com novo content!",
+//     done: true,
+// })
+updateContentById(terceiraTodo.id, "Atualizada")
 console.log(read())
