51306f973ce36f30e783e5ad20e235aaa1bc10eb
Caian Egidio <caianpedroso@gmail.com>
2024-02-05 01:06:30 -0300

    Semanaheho3: final aula 2

--- senamahero3/frontend/src/app/room/[id]/page.tsx
+++ senamahero3/frontend/src/app/room/[id]/page.tsx
@@ -1,12 +1,13 @@
 'use client'
-import { useContext, useEffect } from 'react';
+import { useContext, useEffect, useRef } from 'react';
 import Chat from '@/components/Chat';
 import Footer from '@/components/Footer';
 import Header from '@/components/Header';
 import { SocketContext } from '@/contexts/SocketContext';
 
 export default function Room({ params }: { params: { id: string } }) {
-  const { socket } = useContext(SocketContext)
+  const { socket } = useContext(SocketContext);
+  const localStream = useRef<HTMLVideoElement>(null)
   useEffect(() => {
     socket?.on('connect', async () => {
       console.log('connection');
@@ -14,8 +15,21 @@ export default function Room({ params }: { params: { id: string } }) {
           roomId: params.id,
           socketId: socket.id
       })
-    })
+      await initCamera();
+    });
+
   }, [socket]);
+
+  const initCamera = async () => {
+    const video = await navigator.mediaDevices.getUserMedia({
+      video: true,
+      audio: {
+        noiseSuppression: true,
+        echoCancellation: true,
+      }
+    })
+    if(localStream.current) localStream.current.srcObject = video;
+  }
   
   return (
     <div className="h-mas screen">
@@ -24,7 +38,7 @@ export default function Room({ params }: { params: { id: string } }) {
         <div className="md:w-[85%] w-full m-3 ">
           <div className="grid md:grid-cols-2 grid-cols-1 gap-8">
             <div className="bg-gray-950 w-full rounded-md h-full p-2 relative ">
-              <video className="h-full w-full"></video>
+              <video className="h-full w-full" autoPlay ref={localStream}/>
               <span className="absolute bottom-3">Alexia Kattah</span>
             </div>
             <div className="bg-gray-950 w-full rounded-md h-full p-2 relative ">
--- senamahero3/frontend/src/components/Join.tsx
+++ senamahero3/frontend/src/components/Join.tsx
@@ -1,17 +1,28 @@
 'use client';
-import { useRef } from 'react';
+import { FormEvent, useRef } from 'react';
 import Button from './Button';
 import { Input } from './Input';
+import { useRouter } from 'next/navigation';
 
 export default function JoinRoom() {
   const name = useRef<HTMLInputElement>(null);
   const id = useRef<HTMLInputElement>(null);
+  const router = useRouter();
+  const handleJoinRoom = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if(name.current && name.current?.value !== '' && id.current && id.current.value !== '') {
+      sessionStorage.setItem('username', name.current.value);
+      const roomId = id.current.value;
+      window.location.href = `/room/${roomId}`
+    }
+  }
+
   return (
-    <>
+    <form onSubmit={(e) => handleJoinRoom(e)} className='space-y-8'>
       <Input placeholder="Seu nome" type="text" ref={name} />
       <Input placeholder="ID da reunião" type="text" ref={id} />
 
-      <Button title="Entrar" type="button" />
-    </>
+      <Button title="Entrar" type="submit" />
+    </form>
   );
 }
