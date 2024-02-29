5162538e25920df01e6c0bb7223d5ba98f7e6f42
Caian Egidio <caianpedroso@gmail.com>
2024-02-28 22:51:55 -0300

    Atualizar o todo.ts

--- crudcomqualidade/Modulo-6/src/ui/controller/todo.ts
+++ crudcomqualidade/Modulo-6/src/ui/controller/todo.ts
@@ -4,14 +4,14 @@ import { z as schema } from "zod";
 
 interface TodoControllerGetParams {
     page: number;
-}
+};
 
 async function get(params: TodoControllerGetParams) {
     return todoRepository.get({
         page: params.page,
         limit: 1,
     });
-}
+};
 
 function filterTodosByContent<Todo>(
     search: string,
@@ -23,20 +23,21 @@ function filterTodosByContent<Todo>(
         return contentNormalized.includes(searchNormalized);
     });
     return homeTodos;
-}
+};
 
 interface TodoControllerCreatetParams {
     content?: string;
     onSuccess: (todo: Todo) => void;
     onError: (customMessage?: string) => void;
-}
+};
+
 function create({ content, onSuccess, onError }: TodoControllerCreatetParams) {
     // Fail Fast
     const parsedParams = schema.string().nonempty().safeParse(content);
     if (!parsedParams.success) {
         onError();
         return;
-    }
+    };
 
     todoRepository
         .createByContent(parsedParams.data)
@@ -46,13 +47,13 @@ function create({ content, onSuccess, onError }: TodoControllerCreatetParams) {
         .catch(() => {
             onError();
         });
-}
+};
 
 interface TodoControllerToggleDoneParams {
     id: string;
     onError: () => void;
     updateTodoOnScreen: () => void;
-}
+};
 
 function toggleDone({
     id,
