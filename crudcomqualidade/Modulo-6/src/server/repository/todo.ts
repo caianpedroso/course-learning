d841e4eee59d6338d5d0fe70198be9177d1dfb60
Caian Egidio <caianpedroso@gmail.com>
2024-06-04 23:26:00 -0300

    Atualizar o todo.ts

--- crudcomqualidade/Modulo-6/src/server/repository/todo.ts
+++ crudcomqualidade/Modulo-6/src/server/repository/todo.ts
@@ -11,13 +11,13 @@ import { supabase } from "@server/infra/db/supabase";
 interface TodoRepositoryGetParams {
     page?: number;
     limit?: number;
-}
+};
 
 interface TodoRepositoryGetOutput {
     todos: Todo[];
     total: number;
     pages: number;
-}
+};
 
 async function get({
     page,
