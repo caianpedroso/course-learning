22c7b2176d92dbe8f20a1843d9144948dfac2a72
Caian Egidio <caianpedroso@gmail.com>
2024-07-27 20:10:30 -0300

    metodo-tajs: init modulo 3

--- metodo-tajs/modulo03-conceitos-tajs/aula01-mocks/src/person.js
+++ metodo-tajs/modulo03-conceitos-tajs/aula01-mocks/src/person.js
@@ -1,5 +1,6 @@
 class Person {
-  constructor(name, age) {
-
+  static validate(person) {
+    if(!person.name) throw new Error('name i required')
+    if(!person.cpf) throw new Error('cpf i required')
   }
 }
