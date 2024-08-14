4ca566a7bad2a76b2fcdaf4e71ee9d2e7f5a908d
Caian Egidio <caianpedroso@gmail.com>
2024-08-13 23:16:18 -0300

    metodo-tajs: init modulo 3

--- metodo-tajs/modulo03-conceitos-tajs/aula01-mocks/src/person.js
+++ metodo-tajs/modulo03-conceitos-tajs/aula01-mocks/src/person.js
@@ -1,8 +1,10 @@
 class Person {
+
   static validate(person) {
     if(!person.name) throw new Error('name i required')
     if(!person.cpf) throw new Error('cpf i required')
   }
+
   static format(person) {
     const [name, ...lastName] = person.name.split(' ')
     return {
