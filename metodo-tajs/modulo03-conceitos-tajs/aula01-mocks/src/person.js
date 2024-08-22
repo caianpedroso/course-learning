e839437221b2597d71ea9f1a350429c9b54ad746
Caian Egidio <caianpedroso@gmail.com>
2024-08-21 22:34:04 -0300

    metodo-tajs: init modulo 3

--- metodo-tajs/modulo03-conceitos-tajs/aula01-mocks/src/person.js
+++ metodo-tajs/modulo03-conceitos-tajs/aula01-mocks/src/person.js
@@ -4,6 +4,7 @@ class Person {
     if(!person.name) throw new Error('name i required')
     if(!person.cpf) throw new Error('cpf i required')
   }
+
   static format(person) {
     const [name, ...lastName] = person.name.split(' ')
     return {
@@ -12,12 +13,14 @@ class Person {
       lastName: lastName.join(' ')
     }
   }
+
   static save(person) {
     if(!['cpf', 'name', 'lastName'].every(prop => person[prop])) {
       throw new Error(`cannot save invalid person: ${JSON.stringify(person)}`)
     }
     console.log('registrado com sucesso!', person)
   }
+  
   static process(person) {
     this.validate(person)
     const personFormatted = this.format(person)
