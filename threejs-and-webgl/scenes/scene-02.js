076b101b3d54b8e1d970ee132e88d1279c47f239
Caian Egidio <caianpedroso@gmail.com>
2024-11-18 23:00:54 -0300

    Threejs-and-webgl: modulo 3 aula 2

--- threejs-and-webgl/scenes/scene-02.js
+++ threejs-and-webgl/scenes/scene-02.js
@@ -3,12 +3,36 @@ const material = new THREE.MeshLambertMaterial(
 );
 
 const cube = new THREE.Mesh(
-  new THREE.BoxBufferGeometry(),
+  new THREE.BoxBufferGeometry(
+    1, 1, 1
+  ),
   material
 );
 
 scene.add(cube);
 
+const cicle = new THREE.Mesh(
+  new THREE.CircleBufferGeometry(
+    0.5, 20
+  ),
+  material
+);
+cicle.position.x = -2;
+cicle.rotation.x = THREE.MathUtils.degToRad(-90);
+scene.add(cicle);
+
+const cone = new THREE.Mesh(
+  new THREE.ConeBufferGeometry(
+    0.3, 0.5
+  ),
+  material
+);
+cone.position.x = -2;
+cone.position.y = 2;
+scene.add(cone);
+
+x3.add(cone, { label: 'cone'});
+x3.add(cicle, { label: 'circle'});
 x3.add(cube, { label: 'cube'});
 
 renderer.setAnimationLoop(() => {
