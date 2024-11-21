ab8f3d94d2b0bd7cc8da279de9db41c8c64cd4d9
Caian Egidio <caianpedroso@gmail.com>
2024-11-20 22:27:18 -0300

    Threejs-and-webgl: modulo 3 aula 3

--- threejs-and-webgl/scenes/scene-02.js
+++ threejs-and-webgl/scenes/scene-02.js
@@ -31,6 +31,37 @@ cone.position.x = -2;
 cone.position.y = 2;
 scene.add(cone);
 
+const cylinder = new THREE.Mesh(
+  new THREE.CylinderBufferGeometry(
+    0.5, 0.5, 1
+  ),
+  material
+);
+cylinder.position.x = 2;
+cylinder.position.y = 0;
+scene.add(cylinder);
+
+const plane = new THREE.Mesh(
+  new THREE.PlaneBufferGeometry(
+    1, 1
+  ),
+  material
+);
+plane.position.x = 2;
+plane.position.y = 2;
+plane.rotation.x = THREE.MathUtils.degToRad(-90);
+scene.add(plane);
+
+const sphere = new THREE.Mesh(
+  new THREE.SphereBufferGeometry(
+    0.3, 20, 20
+  ),
+  material
+);
+sphere.position.x = 0;
+sphere.position.y = 2;
+scene.add(sphere);
+
 x3.add(cone, { label: 'cone'});
 x3.add(cicle, { label: 'circle'});
 x3.add(cube, { label: 'cube'});
