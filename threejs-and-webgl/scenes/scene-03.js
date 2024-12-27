237c27399d2e5e220e4d1ee4e928ce0513eb922b
Caian Egidio <caianpedroso@gmail.com>
2024-12-26 23:47:32 -0300

    Threejs-and-webgl: modulo 3 aula 9

--- threejs-and-webgl/scenes/scene-03.js
+++ threejs-and-webgl/scenes/scene-03.js
@@ -1,12 +1,12 @@
 const path = new THREE.Path();
 
 path.moveTo(0.3, 1.5);
-path.lineTo(0.9, 2.2);
-path.lineTo(1.4, 1.7);
-path.lineTo(1.9, 2.2);
-path.lineTo(2.5, 1.5);
-path.lineTo(1.4, 0.3);
-path.lineTo(0.3, 1.5);
+path.quadraticCurveTo(0.3,2.2,0.9, 2.2);
+path.quadraticCurveTo(1.3, 2.2,1.4, 1.7);
+path.quadraticCurveTo(1.5, 2.2,1.9, 2.2);
+path.quadraticCurveTo(2.5, 2.2, 2.5, 1.5);
+path.quadraticCurveTo(2.5,1.0, 1.4, 0.3);
+path.quadraticCurveTo(0.3, 1.0,0.3, 1.5);
 
 const geometry = new THREE.BufferGeometry();
 geometry.setFromPoints(path.getPoints());
