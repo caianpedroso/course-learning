bab92e34c0743d43b54d872f480856e48346975c
Caian Egidio <caianpedroso@gmail.com>
2025-02-23 12:53:19 -0300

    Webpack-do-basico-avancado: modulo 4 plugins add DefinePlugin

--- webpack-do-basico-avancado/3_plugins/src/index.js
+++ webpack-do-basico-avancado/3_plugins/src/index.js
@@ -2,4 +2,7 @@ import Heading from "./components/heading/heading";
 
 const heading = new Heading();
 
-heading.create("Pagina principal")
+heading.create("Pagina principal");
+
+console.log(VERSION);
+console.log(PORT);
