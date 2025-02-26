e354e51ae4d966f1eb724cf396baf8e91ccbe595
Caian Egidio <caianpedroso@gmail.com>
2025-02-26 16:21:02 -0300

    Webpack-do-basico-avancado: modulo 6 integration add fontes de icones

--- webpack-do-basico-avancado/5_integracoes/src/index.js
+++ webpack-do-basico-avancado/5_integracoes/src/index.js
@@ -4,6 +4,14 @@ import './fonts.css';
 // import jquery
 import $ from 'jquery';
 
+// import Font Awesome
+import { library, dom } from '@fortawesome/fontawesome-svg-core';
+import { faBacon, faAnchor } from  '@fortawesome/free-solid-svg-icons';
+
+library.add(faBacon);
+library.add(faAnchor);
+dom.watch();
+
 var body = $('body');
 
 const p = $("<p></p>").text("Inserindo texto").css("color", "red");
