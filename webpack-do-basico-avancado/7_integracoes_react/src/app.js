fcdc3f12fe98cb871baa26eeb71887cefc1ad6b3
Caian Egidio <caianpedroso@gmail.com>
2025-02-27 23:56:02 -0300

    Webpack-do-basico-avancado: modulo 6 integration add react

---
+++ webpack-do-basico-avancado/7_integracoes_react/src/app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Meu projeto em React e Webpaack</h1>
+        <p>Integrando com sucesso!</p>
+      </div>
+    )
+  }
+}
+
+export default App;
