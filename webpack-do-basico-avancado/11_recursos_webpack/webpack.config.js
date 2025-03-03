8c07fab407a93c002d92b0cfec4e4bc85d2a91f5
Caian Egidio <caianpedroso@gmail.com>
2025-03-03 19:00:00 -0300

    Webpack-do-basico-avancado: modulo 7 best perfomace

--- webpack-do-basico-avancado/11_recursos_webpack/webpack.config.js
+++ webpack-do-basico-avancado/11_recursos_webpack/webpack.config.js
@@ -1,23 +1,34 @@
 const path = require("path");
 const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: "bundle.js",
+    filename: "[name].js",
     path: path.resolve(__dirname, "./dist"),
   },
   plugins: [
     new webpack.ProvidePlugin({
       _: 'lodash'
+    }),
+    new HtmlWebpackPlugin({
+      title: 'Recursos Webpack'
     })
   ],
+  devServer: {
+    historyApiFallback: true,
+  },
   module: {
     rules: [
       {
         test: /\.js$/,
         loader: "babel-loader",
+        include: path.resolve(__dirname, "./src"),
       }
     ]
+  },
+  optimization: {
+    runtimeChunk: true
   }
 }
