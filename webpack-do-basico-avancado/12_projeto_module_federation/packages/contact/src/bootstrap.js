0b22432e1a15b0f6ace220d6d6db69e6e4705978
Caian Egidio <caianpedroso@gmail.com>
2025-03-06 10:01:34 -0300

    Webpack-do-basico-avancado: modulo 8 module federation create contact

---
+++ webpack-do-basico-avancado/12_projeto_module_federation/packages/contact/src/bootstrap.js
@@ -0,0 +1,11 @@
+import Contact from "./Contact";
+import React from "react";
+import ReactDOM from "react-dom";
+import {createRoot} from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// ReactDOM.render(<App />, document.getElementById("#root"));
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+root.render(<Contact />);
---
+++ webpack-do-basico-avancado/12_projeto_module_federation/packages/contact/webpack.config.js
@@ -0,0 +1,48 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "[name].js",
+    publicPath: "http://localhost:9003/",
+  },
+  mode: "development",
+  devServer: {
+    // contentBase: path.resolve(__dirname, "./dist"),
+    // index: 'index.html',
+    port: 9003,
+    hot: true,
+    historyApiFallback: true,
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".json"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: "./public/index.html",
+      title: 'App'
+    })
+  ]
+}
