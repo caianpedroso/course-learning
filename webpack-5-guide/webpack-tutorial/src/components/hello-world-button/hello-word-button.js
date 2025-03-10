d8492d4791300c3f240fee16d768d16aa51b2bdc
Caian Egidio <caianpedroso@gmail.com>
2025-03-10 18:10:05 -0300

    Webpack-5-guide: modulo 2 add botton

--- webpack-5-guide/webpack-tutorial/src/components/hello-world-button/hello-word-button.js
+++ webpack-5-guide/webpack-tutorial/src/components/hello-world-button/hello-word-button.js
@@ -1,5 +1,16 @@
-function helloWorld(source) {
-  console.log('Hello World!');
+class HelloWordButton {
+  render() {
+    const button = document.createElement('button');
+    button.innerHTML = 'Hello word';
+    button.classList.add('hello-word-button');
+    const body = document.querySelector('body');
+    button.onclick = function () {
+      const p = document.createElement('p');
+      p.innerText = 'Hello world.';
+      p.classList.add('hello-word-text');
+      body.appendChild(p);
+    }
+    body.appendChild(button);
+  }
 }
-
-export default helloWorld;
+export default HelloWordButton;
--- webpack-5-guide/webpack-tutorial/src/index.js
+++ webpack-5-guide/webpack-tutorial/src/index.js
@@ -1,5 +1,4 @@
-import helloWorld from "./components/hello-world-button/hello-word-button";
-import addImage from "./add-image";
+import HelloWordButton from "./components/hello-world-button/hello-word-button";
 
-helloWorld();
-addImage();
+const helloWordButton = new HelloWordButton();
+helloWordButton.render();
