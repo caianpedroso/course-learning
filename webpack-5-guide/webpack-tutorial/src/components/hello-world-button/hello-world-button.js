5a1e1ba7e1761e9aa31b5dffbe67aecc9a1e2840
Caian Egidio <caianpedroso@gmail.com>
2025-03-11 18:35:05 -0300

    Webpack-5-guide: modulo 2 add loader scss fix

--- webpack-5-guide/webpack-tutorial/src/components/hello-world-button/hello-world-button.js
+++ webpack-5-guide/webpack-tutorial/src/components/hello-world-button/hello-world-button.js
@@ -3,15 +3,15 @@ import './hello-world-button.scss';
 class HelloWorldButton {
   render() {
     const button = document.createElement('button');
-    button.innerHTML = 'Hello word';
-    button.classList.add('hello-world-button');
     const body = document.querySelector('body');
+    button.innerHTML = 'Hello word';
     button.onclick = function () {
       const p = document.createElement('p');
       p.innerText = 'Hello world.';
       p.classList.add('hello-world-text');
       body.appendChild(p);
     }
+    button.classList.add('hello-world-button');
     body.appendChild(button);
   }
 }
