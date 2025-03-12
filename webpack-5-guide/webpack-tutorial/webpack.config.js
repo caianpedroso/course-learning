d8880f535e27d9379caf858a6d4830390404291b
Caian Egidio <caianpedroso@gmail.com>
2025-03-12 18:10:05 -0300

    Webpack-5-guide: modulo 3 add Plugin MiniCssExtractPlugin

--- webpack-5-guide/webpack-tutorial/webpack.config.js
+++ webpack-5-guide/webpack-tutorial/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const TerserPlugin = require('terser-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -30,12 +31,13 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
+        use: [
+          MiniCssExtractPlugin.loader , 'css-loader'],
       },
       {
         test: /\.scss$/,
         use: [
-          'style-loader', 'css-loader', 'sass-loader'
+          MiniCssExtractPlugin.loader , 'css-loader', 'sass-loader'
         ],
       },
       {
@@ -52,6 +54,9 @@ module.exports = {
     ]
   },
   plugins: [
-    new TerserPlugin()
+    new TerserPlugin(),
+    new MiniCssExtractPlugin({
+      filename: 'style.css',
+    })
   ]
 }
